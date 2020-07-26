236897d8e52d6e8a806e5fba341db1e1bee41d06
RyoSci <ryo.saeki.physics@gmail.com>
2020-07-26 19:04:43 +0900

    ABC 146 c.cpp 途中

--- AtCoder_cpp/ABC146/c.cpp
+++ AtCoder_cpp/ABC146/c.cpp
@@ -1,2 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main() {
+    int a, b, x;
+    cin >> a >> b >> x;
+    int n, res_n = 0;
+    n = pow(10, 9);
+    cout << n << endl;
+    if (a * n + b * 10 <= x) {
+        cout << n << endl;
+    } else {
+        for (int i = 8; i >= 0; i--) {
+            for (int j = 9; j >= 0; j--) {
+                n = pow(10, i) * j;
+                if (a * n + b * (i + 1) <= x - res_n) {
+                    res_n += pow(10, i) * j;
+                    break;
+                }
+            }
+        }
+        cout << res_n << endl;
+    }
+}
